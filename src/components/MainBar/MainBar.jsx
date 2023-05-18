0a54092ef8553bd83b50e6ff9db8efd9c7a6069a
Whiz7z <gjevstratov@gmail.com>
2023-05-18 16:47:58 +0100

    first deploy

--- src/components/MainBar/MainBar.jsx
+++ src/components/MainBar/MainBar.jsx
@@ -82,7 +82,23 @@ const MainBar = () => {
     }
   }, [component]);
 
-  return <div className={styles.container}>{componentToDisplay}</div>;
+  return (
+    <div className={styles.container}>
+      {/* <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
+        <filter id="noiseFilter">
+          <feTurbulence
+            type="fractalNoise"
+            baseFrequency="10000000"
+            numOctaves="3"
+            stitchTiles="stitch"
+          />
+        </filter>
+
+        <rect width="100%" height="100%" filter="url(#noiseFilter)" />
+      </svg> */}
+      {componentToDisplay}
+    </div>
+  );
 };
 
 export default MainBar;
